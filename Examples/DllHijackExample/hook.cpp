90a91b3526f986ee214e4b5e031ac1a9eb4b1595
EZForever <34133756+EZForever@users.noreply.github.com>
2025-01-02 23:11:36 +0800

    A bit more info on DllHijackExample

--- Examples/DllHijackExample/hook.cpp
+++ Examples/DllHijackExample/hook.cpp
@@ -1,6 +1,8 @@
 #include "hook.h"
 #include "logging.h"
 
+// You can still define your hook instances like normal
+
 HOOK_INSTANCE(GetSystemTime)
 {
 	VOID WINAPI HandlerRoutine(LPSYSTEMTIME lpSystemTime)
@@ -9,5 +11,18 @@ HOOK_INSTANCE(GetSystemTime)
 
 		LOG(L"[I] GetSystemTime(): Current UTC time is %02u:%02u\n", lpSystemTime->wHour, lpSystemTime->wMinute);
 	}
+}
+
+// But for functions exported by the hijacked DLL, you MUST NOT use HOOK_INSTANCE_* macros
+// Instead, define a redirection in exports.def (as shown there), and define your hook routine under the redirected name
+// Pay attention to function prototype and calling convention as usual, as well as name mangling (use `extern "C"` whenever possible)
+
+extern "C"
+BOOL APIENTRY xGetFileVersionInfoW(LPCWSTR lptstrFilename, DWORD dwHandle, DWORD dwLen, LPVOID lpData)
+{
+	LOG(L"[I] GetFileVersionInfoW(): Called on file \"%s\", dwHandle = 0x%08X\n", lptstrFilename, dwHandle);
+
+	// You can use the original routine directly; delay-load hook got you covered
+	return GetFileVersionInfoW(lptstrFilename, 0, dwLen, lpData);
 }
 
